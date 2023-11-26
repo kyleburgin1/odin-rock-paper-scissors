4e7df29b17f106a13259c1c5e35213bd65284e95
kyleburgin1 <weresyourcape@gmail.com>
2023-11-27 06:46:58 +1100

    starting over saving this point

--- javascript.js
+++ javascript.js
@@ -1,12 +1,51 @@
-function getComputerChoice(){
+const playerText = document.querySelectorAll(".player");
+const computerText = document.querySelectorAll(".computer");
+const resultText = document.querySelectorAll(".result");
+const gameBtns = document.querySelectorAll(".button");
+let player;
+let computer;
+let result;
 
+gameBtns.forEach(button => button.addEventListener("click", () =>{
+    player = button.textContent
+    computerChoice()
+    playerText.textContent = `player: ${player}`;
+    computerText.textContent = `computer: ${computer}`
+    resultText.textContent = playRound();
+    
+}))
+
+
+
+function getComputerChoice(){
     let computerChoice = Math.floor(Math.random() * 3) + 1;
 
-    return computerChoice;
+    switch(computerChoice){
+        case 1:
+            computer = "ROCK"
+            break;
+        case 2:
+            computer = "SCISSORS"
+            break;
+        case 3:
+            computer = "PAPER"
+            break;           
+    }
 }
 
 console.log(getComputerChoice())
 
-function playRound(computerChoice, playerChoice){
-
+function playRound(){
+    if(player == computer){
+        return "Draw!"
+    }
+    else if(computer == "ROCK"){
+        return(player == "PAPER") ? "You Win!" : "You Lose!";
+    }
+    else if(computer == "paper"){
+        return(player == "SCISSORS") ? "You Win!" : "You Lose!";
+    }
+    else if(computer == "SCISSORS"){
+        return(player == "ROCK") ? "You Win!" : "You Lose!";
+    }
 }
