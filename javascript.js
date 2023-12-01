597dbab6fb8dff932791865eee2b07e8f3f39d01
kyleburgin1 <weresyourcape@gmail.com>
2023-12-02 08:07:10 +1100

    javascript file had to be linked under html elements?

--- javascript.js
+++ javascript.js
@@ -1,22 +1,21 @@
 const choiceBtns = document.querySelectorAll(".choiceBtns");
 const playerText = document.querySelector(".player")
 const computerText = document.querySelector(".computer")
-const resultText = document.querySelector(".results")
-let result;
+const resultText = document.querySelector(".result")
 let computer;
 let player
 
 
 choiceBtns.forEach(button => button.addEventListener("click", () => {
-    player = playerText;
-    computer = computerChoice();
-
-
+    player = button.textContent;
+    computerChoice();
+    playerText.textContent = `player: ${player}`;
+    computerText.textContent = `computer: ${computer}`;
+    resultText.textContent = checkWinner();
 }))
 
-
-
 function computerChoice(){
+
     const computerIndex = Math.floor(Math.random() * 3)
     
     switch(computerIndex){
@@ -33,3 +32,18 @@ function computerChoice(){
     }
 }
 
+
+function checkWinner(){
+    if( player === computer){
+        return "It's a draw!";
+    }
+    else if(computer == "ROCK"){
+        return(player == "PAPER") ? "You Win!" : "You Lose!";
+    }
+    else if(computer == "PAPER"){
+        return(player == "SCISSORS") ? "You Win!" : "You Lose!";
+    }
+    else if(computer == "SCISSORS"){
+        return(player == "ROCK") ? "You Win!" : "You Lose!";
+    }
+}
