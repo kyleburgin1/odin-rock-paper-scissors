00f7c7c07694c01403642e4f75627a440df429d0
kyleburgin1 <weresyourcape@gmail.com>
2023-12-02 07:55:20 +1100

    refactoring a lot of code as i got lost with what i had before

--- javascript.js
+++ javascript.js
@@ -1,58 +1,35 @@
-const choiceBtnRock = document.querySelector(".choiceBtnRock");
-const choiceBtnPaper = document.querySelector(".choiceBtnPaper");
-const choiceBtnScissors = document.querySelector(".choiceBtnScissors");
-const playerText = document.querySelector(".player");
-const computerText = document.querySelector(".computer");
+const choiceBtns = document.querySelectorAll(".choiceBtns");
+const playerText = document.querySelector(".player")
+const computerText = document.querySelector(".computer")
 const resultText = document.querySelector(".results")
-let player;
-let computer;
 let result;
+let computer;
+let player
 
-choiceBtns.forEach(button => button.addEventListener("click", () =>{
-    player = button.textContent;
-    computerChoice()
-    playerText.textContent = `Player: ${player}`;
-    computerText.textContent = `computer: ${computer}`;
-    resultText = playRound();
+
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+    player = playerText;
+    computer = computerChoice();
+
+
 }))
 
 
 
 function computerChoice(){
+    const computerIndex = Math.floor(Math.random() * 3)
+    
+    switch(computerIndex){
+        case 1:
+            computer = "ROCK"
+            break;
+        case 2:
+            computer = "PAPER"
+            break;
+        case 3:
+        computer = "SCISSORS" 
+        break;
 
-    let choices = ["ROCK", "PAPER", "SCISSORS"]
-    const computerChoice = Math.floor(Math.random() * 3)
-    return choices[computerChoice];
-}
-
-function playRound(){
-
-
-
-
-    if(player === computer){
-        resultText.innerHTML = "Draw!"
-    }
-    else if (
-        (player === "ROCK" && computer === "SCISSORS") ||
-        (player === "PAPER" && computer === "ROCK") ||
-        (player === "SCISSORS" && computer === "PAPER")
-    ){
-        resultText.innerHTML = "You Win!";
-    }
-    else{
-        resultText.innerHTML = "You Lose!"
-    }
-}
- 
-/*
-function game(){
-    for(let i = 0; i < 5; i++){
-        const playerSelection = prompt("Rock Paper or Scissors?");
-        const computerSelection = computerChoice(); 
-        console.log(playRound(playerSelection, computerSelection));
     }
 }
 
-game();
-*/
